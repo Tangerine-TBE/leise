79705fe0b14d375330bbdb2e30225c4807ff97a6
DESKTOP-N06S4FK\DELL <2232113341@qq.com>
2023-05-13 16:23:06 +0800

    push

--- app/src/main/java/cn/com/dihealth/myapplication/MainActivity.kt
+++ app/src/main/java/cn/com/dihealth/myapplication/MainActivity.kt
@@ -1,21 +1,35 @@
 package cn.com.dihealth.myapplication
 
-import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.animation.LinearInterpolator
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
+import androidx.appcompat.app.AppCompatActivity
 import cn.com.dihealth.myapplication.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
        val binding =  ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-     val animator =   ObjectAnimator.ofFloat(binding.customView,"offsetAngle",360f)
-        animator.repeatCount = ValueAnimator.INFINITE
-        animator.interpolator = LinearInterpolator()
-        animator.duration = 10 * 1000
-        animator.start()
+//     val animator =   ObjectAnimator.ofFloat(binding.customView,"offsetAngle",360f)
+//        animator.repeatCount = ValueAnimator.INFINITE
+//        animator.interpolator = LinearInterpolator()
+//        animator.duration = 10 * 1000
+//        animator.start()
+
+        val anim: Animation = RotateAnimation(360f,
+            0f,Animation.RELATIVE_TO_SELF,
+            0.5f,Animation.RELATIVE_TO_SELF,
+            0.5f
+        )
+        anim.fillAfter = true // 设置保持动画最后的状态
+        anim.duration = 10 * 1000 // 设置动画时间
+        anim.repeatCount = ValueAnimator.INFINITE
+        anim.interpolator = AccelerateInterpolator() // 设置插入器
+        binding.customView.startAnimation(anim)
+
     }
 }
